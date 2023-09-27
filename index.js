fe29813bc899dfe5dfa89eeecf886b82e66a3f06
RafaSantana <rafaelsantana.tech@gmail.com>
2023-09-27 14:48:43 -0300

    estrutura if alterada para switch case

--- index.js
+++ index.js
@@ -13,23 +13,32 @@ function calculaRanckPartidas(v, d) {
 
 function calculaElo(v) {
     let elo = ""
-    if (v <= 10) {
-        elo = "Ferro"
-    } else if ((v > 10) && (v <= 20)) {
-        elo = "Bronze"
-    } else if ((v > 20) && (v <= 50)) {
-        elo = "Prata"
-    } else if ((v > 50) && (v <= 80)) {
-        elo = "Ouro"
-    } else if ((v > 80) && (v <= 90)) {
-        elo = "Diamante"
-    } else if ((v > 90) && (v <= 100)) {
-        elo = "Lendário"
-    } else {
+    switch(true) {
+    case (v <= 10):
+      elo = "Ferro"
+      break
+    case (v > 10 && v <= 20):
+      elo = "Bronze"
+      break  
+    case (v > 20 && v <= 50):
+      elo = "Prata"
+      break
+    case (v > 50 && v <= 80):
+      elo = "Ouro"  
+      break
+    case (v > 80 && v <= 90):
+      elo = "Diamante"
+      break
+    case (v > 90 && v <= 100):
+      elo = "Lendário" 
+      break
+      case (v >= 101):
         elo = "Imortal"
+        break;
+      default:
+        console.log("Opção inválida")
     }
-
-    notificacao = ("O Herói tem de saldo de " + v + " está no nível de " + elo)
-    return notificacao
-
-}
+    
+    let notificacao = ("O Herói tem de saldo de vitórias " + v + ", está no nível de " + elo);
+    return notificacao;
+  }
